6017708ba69199501819c016db9669e17118d987
Lavinash <86287101+Lavinash@users.noreply.github.com>
2022-12-14 14:24:45 +0100

    add feature 2
    proposition acitivté selon nombre de personne

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #route pour random activity
 def random_activity():
   activity = requests.get('https://www.boredapi.com/api/activity/').json()
+  return render_template('activity.html', activity=activity)
+
+@app.route('/activity')
+def activity_by_participants():
+  participants = input("Combien de participant serez vous ? (1 à 10)")
+  activity = requests.get(f'https://www.boredapi.com/api/activity?participants={participants}').json()
   return render_template('activity.html', activity=activity)
 
 if __name__ == '__main__':
