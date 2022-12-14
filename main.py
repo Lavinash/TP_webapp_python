9d4b842eb2e26d801c580a785a1849ac0a43764f
root <root@Debian.myguest.virtualbox.org>
2022-12-14 17:14:11 +0100

    fixing html files + main.py

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def index():
     #affiche page accueil avec deux options
     return render_template('index.html')
 
-@app.route('/') #route pour activité random
+@app.route('/activity') #route pour activité random
 def random_activity():
   activity = requests.get('https://www.boredapi.com/api/activity/').json()
   return render_template('activity.html', activity=activity)
 
-@app.route('/activity', methods=['GET', 'POST']) #route pour activité selon le nombre de personne
+@app.route('/activity_form', methods=['GET', 'POST']) #route pour activité selon le nombre de personne
 def activity_by_participants():
-  if request.method == 'POST': #vérification de l'envoi du formulaire
+ if request.method == 'POST': #vérification de l'envoi du formulaire
     participants = request.form['participants']
     activity = requests.get(f'https://www.boredapi.com/api/activity?participants={participants}').json()
     return render_template('activity.html', activity=activity)
-  else: #sinon renvoyer le formulaire
+ else: #sinon renvoyer le formulaire
     return render_template('activity_form.html')
 
 if __name__ == '__main__':
