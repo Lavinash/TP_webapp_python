a4d2098ae9b631b64ec52212bba19fafd82f3822
Lavinash <86287101+Lavinash@users.noreply.github.com>
2022-12-14 14:55:15 +0100

    feature 2 add and fixed

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/') #route pour random activity
+@app.route('/') #route pour activité random
 def random_activity():
   activity = requests.get('https://www.boredapi.com/api/activity/').json()
   return render_template('activity.html', activity=activity)
 
-@app.route('/activity')
+@app.route('/activity', methods=['GET', 'POST']) #route pour activité selon le nombre de personne
 def activity_by_participants():
-  participants = input("Combien de participant serez vous ? (1 à 10)")
-  activity = requests.get(f'https://www.boredapi.com/api/activity?participants={participants}').json()
-  return render_template('activity.html', activity=activity)
+  if request.method == 'POST': #vérification de l'envoi du formulaire
+    participants = request.form['participants']
+    activity = requests.get(f'https://www.boredapi.com/api/activity?participants={participants}').json()
+    return render_template('activity.html', activity=activity)
+  else: #sinon renvoyer le formulaire
+    return render_template('activity_form.html')
 
 if __name__ == '__main__':
   app.run()
+
+
+  
