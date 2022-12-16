b572949d1c8adbbac95b25ddf672f6c8581a2cb6
Lavinash <86287101+Lavinash@users.noreply.github.com>
2022-12-16 15:05:06 +0100

    create test_app.py
    ajout tests unitaire

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import requests
+from app import app
+
+def test_index(): #test pour l'affichage de la page d'accueil
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"Accueil" in response.data #vérifie que la chaine de caractere soit présente dans les données de la réponse
+
+def test_random_activity(): #test pour l'affichage de la page d'activité aléatoire
+    response = app.test_client().get('/activity')
+    assert response.status_code == 200
+    assert b"Activité aléatoire" in response.data
+
+def test_activity_form():     #test pour l'affichage de la page formulaire
+    response = app.test_client().get('/activity_form')
+    assert response.status_code == 200
+    assert b"Choisissez le nombre de personnes" in response.data
