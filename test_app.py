d4f67da58595fd80845258f583fecde28b293927
Lavinash <86287101+Lavinash@users.noreply.github.com>
2022-12-22 11:26:52 +0100

    update test_app avec nouvelle methode

--- test_app.py
+++ test_app.py
@@ -1,18 +1,25 @@
+import pytest
 import requests
+from main import app
 
-url = "http://localhost:5000"
+@pytest.fixture #declaration de fonction de configuration pour les fonctions de tests
+def client(): #creation environnement de test
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
 
-def test_index(): #test pour l'affichage de la page d'accueil
-    response = requests.get(url)
+    
+def test_index_page(client): #test pour l'affichage de la page d'accueil
+    response = client.get('/')
     assert response.status_code == 200
-    assert b"Accueil" in response.data #vérifie que la chaine de caractere soit présente dans les données de la réponse
+    assert b'index.html' in response.data
 
-#def test_random_activity(): #test pour l'affichage de la page d'activité aléatoire
-#    response = app.test_client().get('/activity')
-#    assert response.status_code == 200
-   # assert b"Activité aléatoire" in response.data
+def test_random_activity(client): #test pour l'affichage de la page d'activité aléatoire
+    response = client.get('/activity')
+    assert response.status_code == 200
+    assert b'activity.html' in response.data
 
-#def test_activity_form():     #test pour l'affichage de la page formulaire
-#    response = app.test_client().get('/activity_form')
-#    assert response.status_code == 200
-#    assert b"Choisissez le nombre de personnes" in response.data
+#def test_activity_by_participants(client):
+  #  response = client.post('/activity_form', data={'participants': 2})
+  #  assert response.status_code == 200
+ #   assert b'activity.html' in response.data
