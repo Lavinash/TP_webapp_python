8d2cc56626b838e9df9ef614191c20679bf70829
Lavinash <86287101+Lavinash@users.noreply.github.com>
2022-12-24 18:50:45 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -19,7 +19,7 @@ def test_random_activity(client): #test pour l'affichage de la page d'activité 
     assert response.status_code == 200
     assert b'Aleatoire' in response.data
 
-#def test_activity_by_participants(client):
-  #  response = client.post('/activity_form', data={'participants': 2})
-  #  assert response.status_code == 200
- #   assert b'activity.html' in response.data
+def test_activity_by_participants(client): #test pour l'affichage de la page activité selon le nb de personnes
+    response = client.post('/activity_form', data={'participants': 2}) #on test avec une input de 2 participants
+    assert response.status_code == 200
+    assert b'activity.html' in response.data
